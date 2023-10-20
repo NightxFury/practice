7561a11caf64340330f8a468b28f922f13e8523c
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-21 00:29:50 +0530

    Add files via upload

---
+++ practice19.c
@@ -0,0 +1,13 @@
+/*
+#include <stdio.h>
+int main () {
+    int a; // side = a
+    printf("Enter the value of a");
+    scanf("%d",&a);
+
+    int A = a*a;
+    printf("A is %d", A);
+    return 0;
+
+}
+ */
