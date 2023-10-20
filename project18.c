ab78ab8d75bada6ed1c1295e8a8e11651c81906f
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-21 00:31:48 +0530

    Add files via upload

---
+++ project18.c
@@ -0,0 +1,17 @@
+
+/*
+#include <stdio.h>
+int main () {
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+
+    printf("enter b");
+    scanf("%d",&b);
+
+    int sum = a+b;5
+    printf("sum of %d \n",sum);
+
+    return 0;
+}
+ */
