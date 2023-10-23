67f9e0ba0dbfca4d82551ebe7c872b562cf644a0
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-23 18:50:28 +0530

    Add files via upload

---
+++ project23.c
@@ -0,0 +1,15 @@
+/*
+#include <stdio.h>
+int main () {
+    int a,b;
+    printf("enter the a");
+    scanf("%d",&a);
+
+    printf("enter the b");
+    scanf("%d", &b);
+
+    int P = a - b;
+    printf("P is %d", P);
+    return 0 ;
+}
+*/
