29594c0c390169ef081fbc6144f7f86731287e49
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-23 18:51:10 +0530

    Add files via upload

---
+++ practice23.c
@@ -0,0 +1,16 @@
+/*
+#include <stdio.h>
+int main () {
+    int a,b;
+    printf("enter the value of a");
+    scanf("%d",&a);
+
+    printf("enter the value of b");
+    scanf("%d",&b);
+
+    int area = 1/2 * a * b;
+    printf("area of %f",area);
+
+    return 0;
+}
+ */
