f5c9df40e5bbbee80bf83e41b683239f329e839f
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-23 18:51:48 +0530

    Add files via upload

---
+++ project26.c
@@ -0,0 +1,17 @@
+/*
+
+#include <stdio.h>
+int main () {
+    for (int i = 5; i <= 50; i++){
+        if(i % 2!= 0)//case of odd numbers
+            printf("%d \n", i);
+
+
+
+    }
+
+
+    return 0;
+
+}
+ */
