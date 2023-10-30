2c1dc65c000af0847f9aaf3ba2edfd3945f3bdf2
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-30 21:40:07 +0530

    Add files via upload

---
+++ practice31.c
@@ -0,0 +1,12 @@
+/*
+#include <stdio.h>
+int main () {
+    int marks;
+    printf("enter your marks");
+    scanf("%d",&marks);
+
+    marks <= 30 && marks > 100 ? printf("Pass \n") : printf("Fail \n");
+
+    return 0;
+}
+ */
