bd265d2b851d45de238906f401fd5f22614ed802
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-30 21:40:28 +0530

    Add files via upload

---
+++ practice32.c
@@ -0,0 +1,17 @@
+/*
+#include <stdio.h>
+int main () {
+    int n;
+    printf("Enter the number :");
+    scanf("%d",&n);
+
+    int fac = 1;
+    for (int i = 1; i <= n; ++i) {
+        fac = fac * i;
+
+    }
+    printf("final fac is %d", fac);
+
+    return 0;
+}
+*/
