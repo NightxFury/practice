61a030e5bfe2700a05cb6cffd9ac06075dd231ee
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-31 23:39:31 +0530

    Add files via upload

---
+++ practice37.c
@@ -0,0 +1,19 @@
+/*
+#include <stdio.h>
+
+int calculatepercentage (int s ,int m, int S);
+
+int main (){
+    int s = 94;
+    int m = 98;
+    int S= 78;
+
+    printf("percentage id %d", calculatepercentage(s,m,S));
+
+    return 0;
+}
+
+int calculatepercentage(int s, int m, int S) {
+    return (( s + m + S)/3);
+}
+ */
